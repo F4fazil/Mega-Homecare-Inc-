d2919762d78da02436429b8fc05f46258b90d4fe
fazil <qazifazil4@gmail.com>
2026-08-05 06:46:26 +0500

    latest

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  apkUrl: "/megahomecare.apk",
+  apkUrl: "https://firebasestorage.googleapis.com/v0/b/mega-h.firebasestorage.app/o/app-release.apk?alt=media&token=037eef7a-9877-4e3d-bd31-81c5a0fcb525",
   iosEnabled: false,
   companyName: "MegaHome Care Inc.",
   appName: "MegaHome Care",
